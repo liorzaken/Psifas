ef8a6526fadb91ffa98f9f427be67895eb675148
moti <motimonso@gmail.com>
2015-02-10 06:44:30 -0400

    fix session security

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,16 @@
 class ApplicationController < ActionController::Base
-  before_filter :session_read
-  def session_read
-    @current_user = session[:user_name]
-    @admin = session[:admin]
-  end
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
+  before_filter :session_read
+  
+  def session_read
+    if session[:user_name]===""
+      redirect_to root_path
+    end
+    @current_user = session[:user_name]
+    @admin = session[:admin]
+    
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,9 @@
 class SessionsController < ApplicationController
   layout "login"
+  skip_before_filter :session_read
   def new
   end
   
-  
-  
-
   def create
     user = User.find_by_userName(params[:userName])
     if user && user.authenticate(params[:password])
@@ -16,10 +14,11 @@ class SessionsController < ApplicationController
       flash.now.alert = "Invalid email or password"
       render "new"
     end
+    
   end
 
   def destroy
-    session[:user_name] = nil
-    redirect_to new_session_path
+    session[:user_name] = ""
+    redirect_to root_path
   end
 end
